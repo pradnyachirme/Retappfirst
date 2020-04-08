a529cd84b3a4c1fbb2e05932ec35c7a38f59b0d4
pradnyachirme <chirmepradnya@gmail.com>
2020-04-08 22:20:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/retappfirst/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.example.retappfirst.constant;
+
+public class Constant {
+    public static class baseUrl{
+        public final static String BASE_URL = "https://pradnyac.000webhostapp.com/user_retro_testing/";
+    }
+}
